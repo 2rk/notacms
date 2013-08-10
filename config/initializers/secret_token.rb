6f3f56b7dd6cf05f77226054235074eb7a1fdc29
Nigel Rausch <nigelr@brisbanerails.com>
2013-08-10 16:11:32 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notacms::Application.config.secret_token = 'caa02b933fa447aee008d94926c19a309b7dadf657d7127f9963ec2fad2933b02f221b4b21d0209df5abde8a40457f02370fbb6f76fe8c2b21ca9cf51cdef7d2'
