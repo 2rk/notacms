dd0dd2e4ddd20f825f4792a45e78f8407b5f9385
Nigel Rausch <nigelr@brisbanerails.com>
2013-10-07 07:44:27 +1000

    notacms-3 make open pages reload on page opening

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -2,6 +2,6 @@ class ViewsController < ApplicationController
   def index
     @user = User.find_or_create_by_id(cookies[:notacms])
     cookies[:notacms] = @user.id
-    @pages = []
+    @pages = @user.positions.map {|position| Page.get_meme(position.page, cookies[:notacms])}
   end
 end
